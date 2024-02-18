de11b93b7535835b600637df840697b79e8dc524
Naveen-2207 <naveebadboy@gmail.com>
2024-02-18 18:09:01 +0530

    Java Script Files

---
+++ java script/own task/day1.js
@@ -0,0 +1,16 @@
+// function create(){
+// let a=document.getElementById("cont").value;
+// let para=document.createElement("p");
+// let butcont=document.createTextNode(a);
+// para.appendChild(butcont);
+// document.body.appendChild(para);
+// } 
+var a=10;
+var b=20;a++
+// alert(a);
+if(a==B){
+    alert(yes);
+
+}else{
+    alert(no);
+}
---
+++ java script/own task/img.js
@@ -0,0 +1,26 @@
+function select1(){
+
+	let a=document.getElementById("getval").value;
+
+	document.getElementById("image").src=a;
+
+		
+	}
+	
+function colorbtn1(){
+
+	document.getElementById("bckcl").style="background-color: red";
+
+	}
+
+function colorbtn2(){
+
+	document.getElementById("bckcl").style="background-color: green";
+
+	}
+
+function colorbtn3(){
+
+	document.getElementById("bckcl").style="background-color: blue";
+
+	}
